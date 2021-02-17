d5c709ec86ef48af61faa04eef19b2a0f47823b5
cybercat37794 <68174387+cybercat37794@users.noreply.github.com>
2021-02-17 19:26:24 +0600

    Update TempControl.cpp
    Temperature Sensing and Control System

--- Temperature-sensing-and-control-system/TempControl.cpp
+++ Temperature-sensing-and-control-system/TempControl.cpp
@@ -1,1 +1,40 @@
+//Temperature Sensing And Control System Codes
+//ARDUINO codes
+int tempC = 0;
 
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  tempC = -40 + 0.488155 * (analogRead(A0) - 20);
+  digitalWrite(7, HIGH);
+  if (tempC >= 40) {
+    digitalWrite(8, HIGH);
+    analogWrite(3, 255);
+    delay(4000); // Wait for 4000 millisecond(s)
+    digitalWrite(8, LOW);
+    delay(4000); // Wait for 4000 millisecond(s)
+  }
+  if (tempC < 40 && tempC > 30) {
+    digitalWrite(11, HIGH);
+    analogWrite(3, 128);
+    delay(4000); // Wait for 4000 millisecond(s)
+    digitalWrite(11, LOW);
+    delay(4000); // Wait for 4000 millisecond(s)
+  }
+  if (tempC < 30) {
+    digitalWrite(9, HIGH);
+    analogWrite(3, 0);
+    delay(4000); // Wait for 4000 millisecond(s)
+    digitalWrite(9, LOW);
+    delay(4000); // Wait for 4000 millisecond(s)
+  }
+}
